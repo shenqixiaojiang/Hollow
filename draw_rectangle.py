e658435f1c36360f0b194e35cd32cf29f5cc01e9
shenqixiaojiang <shenshiwei11@163.com>
2019-02-13 11:53:30 +0800

    load the 中文

--- draw_rectangle.py
+++ draw_rectangle.py
@@ -1,8 +1,13 @@
-from PIL import Image,ImageDraw
+#coding:utf-8
+from PIL import Image,ImageDraw,ImageFont
+
+font = ImageFont.truetype('./simkai.ttf', 20) ##load the chinese font
 def draw_rectangle(img_full_path, save_path):
     img = Image.open(img_full_path)
     img = img.resize((299,299))
     drawObject = ImageDraw.Draw(img)
-    left,top,right,bottom = 100,100,200,200
-    drawObject.rectangle([left,top,right,bottom],outline = "red")
+    for ii in range(3):
+        left,top,right,bottom = 100 + ii,100 + ii,200 + ii,200 + ii
+        drawObject.rectangle([left,top,right,bottom],outline = "red")
+        drawObject.text([left, top], '少年加油'.decode('utf-8'), font=font, fill=(0, 255, 0))
     img.save(save_path)
